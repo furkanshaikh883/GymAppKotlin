fa03461cc7b65910394a74971e9e331580dc750d
Sakib <syed.sakib143@gmail.com>
2020-12-15 10:55:49 +0530

    Term and condition added......Sakib Syed

---
+++ app/src/main/java/com/example/gymappkotlin/activity/TermAndCondition.kt
@@ -0,0 +1,14 @@
+package com.example.gymappkotlin.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gymappkotlin.R
+
+class TermAndCondition : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_term_and_condition)
+
+    }
+}
